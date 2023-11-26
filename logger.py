edb6f8d9909cdef10efe222f7399a351b470d367
kishor <kiskumar@radisys.com>
2023-11-26 22:57:03 +0530

    LLD Logger system

---
+++ logger.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+
+class Handler:
+    @abstractmethod
+    def handle(self, level, message):
+        pass
+
+class InfoHandler(Handler):
+    def __init__(self, next):
+        self.next = next
+    
+    def handle(self, level, message):
+        if level.upper() == 'INFO':
+            print(f'{message} : {message}')
+        else:
+            self.next.handle(level, message)
+
+class WarnHandler(Handler):
+    def __init__(self, next):
+        self.next = next
+    
+    def handle(self, level, message):
+        if level.upper() == 'WARN':
+            print(f'{message} : {message}')
+        else:
+            self.next.handle(level, message)
+
+class ErrorHandler(Handler):
+    def __init__(self, next):
+        self.next = next
+    
+    def handle(self, level, message):
+        if level.upper() == 'ERROR':
+            print(f'{level} : {message}')
+        else:
+            self.next.handle(level, message)
+
+class Logger:
+
+    def __init__(self):
+        errorhandler = ErrorHandler(None)
+        warnhandler = WarnHandler(errorhandler)
+        infohandler = InfoHandler(warnhandler)
+        self.handler = infohandler
+
+    def log(self, level, message):
+        self.handler.handle(level, message)
+    
+
+if __name__ == '__main__':
+    logger = Logger()
+    # logger.log("WARN", "Warning")
+    # logger.log("INFO", "Information")
+    logger.log("ERROR", "Error")
